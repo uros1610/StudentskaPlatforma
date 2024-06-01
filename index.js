1566190b0e5653dd110767fbef4ade72bf29377c
Uroš Milović <urkemilovic16@gmail.com>
2024-06-01 15:01:04 +0200

    kreiranje servera

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const http = require('http')
+
+
+const app = express()
+const server = http.createServer(app);
+
+
+
+server.listen(8000,() => {
+    console.log("Connected!");
+})
