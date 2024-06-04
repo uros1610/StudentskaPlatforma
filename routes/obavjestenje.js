703b190918318ffda7e4c0e62a290f83a40e9e4a
Uroš Milović <urkemilovic16@gmail.com>
2024-06-04 23:32:10 +0200

    upiti i ruta za obavjestenja

---
+++ routes/obavjestenje.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const {svaObavjestenjaPredmet,insertObavjestenje,updateObavjestenje} = require('../controllers/obavjestenje')
+
+router.get('/:imePredmeta/:imeSmjera/:imeFakulteta',svaObavjestenjaPredmet);
+
+router.post('/:idProfesora/:imePredmeta/:imeSmjera/:imeFakulteta',insertObavjestenje);
+
+router.put('/:idObavjestenja',updateObavjestenje);
+
+
+module.exports = router;
