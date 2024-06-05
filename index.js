2a843ae35e3c7614c37eeaac981d946bb4b38e04
Uroš Milović <urkemilovic16@gmail.com>
2024-06-05 19:16:13 +0200

    auth ruta

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use('/student',require('./routes/student'));
 app.use('/profesor',require('./routes/profesor'));
 app.use('/obavjestenje',require('./routes/obavjestenje'));
+app.use('/auth',require('./routes/auth'));
 
 
 
