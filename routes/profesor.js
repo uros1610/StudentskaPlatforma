b58fae1ec8acb7573190a79396d34da9a425593d
Uroš Milović <urkemilovic16@gmail.com>
2024-06-05 19:27:18 +0200

    obrisano

--- routes/profesor.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const {sviPredmetiProfesor} = require('../controllers/profesor');
-
-router.get('/sviPredmeti/:id',sviPredmetiProfesor);
-
-
-module.exports = router;
