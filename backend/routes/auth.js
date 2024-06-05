2e03645283baf79603ea9ec7108e1579ef415097
Uroš Milović <urkemilovic16@gmail.com>
2024-06-05 21:31:21 +0200

    dodat login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router();
 
-const {login} = require('../controllers');
+const {login,logout} = require('../controllers/auth');
 
 router.post('/login',login);
+router.post('/logout',logout);
 
 module.exports = router;
