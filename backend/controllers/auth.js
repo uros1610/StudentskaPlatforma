8d30380c0193f85f36a3d0fbfd0f136bbd9d8a08
Uroš Milović <urkemilovic16@gmail.com>
2024-06-24 19:54:05 +0200

    login izmjenjen

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -6,43 +6,48 @@ require('dotenv').config()
 
 const login = (req,res) => {
 
-    const {korisnickoIme,lozinka} = req.body;
+    const {username,password} = req.body;
 
     const queryStudent = "SELECT * From Student WHERE korisnickoIme = ?"
     const queryProfesor = "SELECT * FROM Profesor where korisnickoIme = ?"
 
-    db.query(queryStudent,[korisnickoIme],(err,data) => {
+    db.query(queryStudent,[username],(err,data) => {
         if(err) {
             return res.status(500).json("Internal server error!");
         }
 
         if(data.length) {
-        const correctPassword = bcrypt.compareSync(Lozinka,data[0].lozinka);
+        const correctPassword = bcrypt.compareSync(password,data[0].lozinka);
 
             if(correctPassword) {
-                const token = jwt.sign({korisnickoime:data[0].korisnickoime,indeks_studenta:data[0].indeks_studenta,ime_smjera:data[0].ime_smjera,ime_fakulteta:data[0].ime_fakulteta},process.env.SECRET_KEY);
+                const token = jwt.sign({korisnickoIme:data[0].korisnickoime,indeksStudenta:data[0].indeks_studenta,imeSmjera:data[0].ime_smjera,imeFakulteta:data[0].ime_fakulteta},process.env.SECRET_KEY);
 
-                const sendObj = {korisnickoime:data[0].korisnickoime,indeks_studenta:data[0].indeks_studenta,ime_smjera:data[0].ime_smjera,ime_fakulteta:data[0].ime_fakulteta,rola:"Student",token:token};
+                const sendObj = {korisnickoIme:data[0].korisnickoime,indeksStudenta:data[0].indeks_studenta,imeSmjera:data[0].ime_smjera,imeFakulteta:data[0].ime_fakulteta,rola:"Student",token:token};
 
                 return res.status(200).json(sendObj);
             }
         }
         
 
-        db.query(queryProfesor,[korisnickoIme],(err,data) => {
+        db.query(queryProfesor,[username],(err,data) => {
+
+            if(err) {
+                return res.status(500).json("Internal server error!");
+            }
+
             if(!data.length) {
-                return res.status(401).json("Nepravilno korisnicko ime!");
+                return res.status(401).json({message:"Nepravilno korisničko ime!"});
             }
             else {
-                const correctPassword = bcrypt.compareSync(lozinka,data[0].lozinka)
+                const correctPassword = bcrypt.compareSync(password,data[0].lozinka)
 
                 if(!correctPassword) {
-                    return res.status(401).json("Nepravilna lozinka!");
+                    return res.status(401).json({message:"Nepravilna lozinka!"});
                 }
 
-            const token = jwt.sign({korisnickoime:data[0].korisnickoime_studenta},process.env.SECRET_KEY);
+            const token = jwt.sign({korisnickoIme:data[0].korisnickoime},process.env.SECRET_KEY);
 
-            const sendObj = {korisnickoime:data[0].korisnickoime_studenta,rola:"Profesor",token:token};
+            const sendObj = {korisnickoIme:data[0].korisnickoime,rola:"Profesor",token:token};
 
             return res.status(200).json(sendObj);
             }
