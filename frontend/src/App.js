e1785f877b8c40894b1bda40fd082dab7fb3f230
Uroš Milović <urkemilovic16@gmail.com>
2024-06-25 20:01:14 +0200

    rute

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
+import { Routes,Route} from "react-router-dom";
 import Login from "./components/Login";
-
+import { useState } from "react";
+import ObavjestenjaGlavnaStranica from "./components/ObavjestenjaGlavnaStranica";
+import ObavjestenjaPredmet from "./components/ObavjestenjaPredmet";
 function App() {
+  const [socket,setSocket] = useState(null);
   return (
     <div className="App">
+      <Routes>
+      <Route path = '/' element = {<Login/>}/>
+      <Route path = '/login' element = {<Login/>}/>
+      <Route path = '/notifications' element = {<ObavjestenjaGlavnaStranica/>}/>
+      <Route path = '/notifications/:imePredmeta/:imeSmjera/:imeFakulteta' element = {<ObavjestenjaPredmet/>}/>
 
-      <Login/>
+      </Routes>
       
     </div>
   );
