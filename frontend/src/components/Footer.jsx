8f4562fef1c28d964d744168333eca611e157f78
Tiikss <trakocevic7@gmail.com>
2024-06-25 12:57:28 +0200

    Footer component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,24 @@
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
+import "../styles/footer.css"
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div id="footer-container">
+            <footer id="footer">
+                <p id="footer-mess">&copy; 2024 Univerzitet Crne Gore | Sva prava zadržana | ezIndex</p>
+                <div>
+                    <div id="footer-email-div">
+                        <FontAwesomeIcon icon={faEnvelope} className="footer-icons" />
+                        <p className="footer-p">rektorat@ucg.ac.me</p>
+                    </div>
+                    <div id="footer-phone-div">
+                        <FontAwesomeIcon icon={faPhone} className="footer-icons" />
+                        <p className="footer-p">+382-20-414-255</p>
+                    </div>
+                </div>
+            </footer>
         </div>
     )
 }
