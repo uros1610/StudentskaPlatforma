c99db29b1ae060f02df78032e2d755ae7e548a51
Uroš Milović <urkemilovic16@gmail.com>
2024-06-26 21:53:27 +0200

    ime na header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,18 @@
-import React from "react"
+import React, { useContext } from "react"
 import logo from "../images/logo_si.png"
 import { Link } from "react-router-dom"
 import "../styles/header.css"
+import AuthContext from "../context/AuthContext"
 
 const Header = () => {
+    const {user} = useContext(AuthContext)
     return (
         <div id="hreader-container">
             <header id="header">
                 <nav className="header-nav header-padding-nav">
                     <Link to="/"><img src={logo} alt="Logo" id="header-logo-img" /></Link>
                     <ul>
-                        <li><Link to="/profile">Nalog</Link></li>
+                        <li><Link to="/profile">{user.korisnickoIme}</Link></li>
                         <li><Link to="/login">Odjavi se</Link></li>
                     </ul>
                 </nav>
