405fb137239633cf83f99134b66096d03e5234f6
Uroš Milović <urkemilovic16@gmail.com>
2024-06-26 21:02:05 +0200

    obavjestenja

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Login from "./components/Login";
 import { useState } from "react";
 import ObavjestenjaGlavnaStranica from "./components/ObavjestenjaGlavnaStranica";
 import ObavjestenjaPredmet from "./components/ObavjestenjaPredmet";
+import NovoObavjestenje from "./components/NovoObavjestenje";
 function App() {
   const [socket,setSocket] = useState(null);
   return (
@@ -12,6 +13,8 @@ function App() {
       <Route path = '/login' element = {<Login/>}/>
       <Route path = '/notifications' element = {<ObavjestenjaGlavnaStranica/>}/>
       <Route path = '/notifications/:imePredmeta/:imeSmjera/:imeFakulteta' element = {<ObavjestenjaPredmet/>}/>
+      <Route path = '/newNotification/:imePredmeta/:imeSmjera/:imeFakulteta' element = {<NovoObavjestenje/>}/>
+      <Route path = '/editNotification/:id' element = {<NovoObavjestenje type = "edit"/>}/>
 
       </Routes>
       
