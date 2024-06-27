1696726e5facab1243ebc6bad9feef20f8fb2374
Uroš Milović <urkemilovic16@gmail.com>
2024-06-27 20:17:13 +0200

    malo ispravljena paginacija, da paginira na back-u

--- frontend/src/components/Pagination.jsx
+++ frontend/src/components/Pagination.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Pagination = ({ itemsPerPage, totalItems, paginate, currentPage }) => {
     const pageNumbers = [];
+
+    console.log(itemsPerPage,totalItems);
 
     for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
         pageNumbers.push(i);
@@ -12,9 +15,9 @@ const Pagination = ({ itemsPerPage, totalItems, paginate, currentPage }) => {
             <ul className="pagination">
                 {pageNumbers.map(number => (
                     <li key={number} className={`page-item ${currentPage === number ? 'active' : ''}`}>
-                        <a onClick={() => paginate(number)} href="#" className="page-link">
+                        <Link onClick={() => paginate(number)} href="#" className="page-link">
                             {number}
-                        </a>
+                        </Link>
                     </li>
                 ))}
             </ul>
--- frontend/src/pages/GradeSheet.jsx
+++ frontend/src/pages/GradeSheet.jsx
@@ -5,35 +5,72 @@ import "../styles/pagination.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileLines } from "@fortawesome/free-solid-svg-icons";
 import "../styles/gradesheet.css";
-
-const predmeti = [
-    { id: 1, naziv: "Matematika 1", ocjena: "A", ects: 6 },
-    { id: 2, naziv: "Matematika 2", ocjena: "A" },
-    { id: 3, naziv: "Matematika 3", ocjena: "A" },
-    { id: 4, naziv: "Matematika 4", ocjena: "A" },
-    { id: 5, naziv: "Matematika 5", ocjena: "A" },
-    { id: 6, naziv: "Matematika 6", ocjena: "A" },
-    { id: 7, naziv: "Matematika 7", ocjena: "A" },
-    { id: 8, naziv: "Matematika8",  ocjena: "A" },
-    { id: 9, naziv: "Matematika9",  ocjena: "A" },
-    { id: 10, naziv: "Matematika 10", ocjena: "A" },
-    { id: 11, naziv: "Matematika 11", ocjena: "A" },
-    { id: 12, naziv: "Matematika 12", ocjena: "A" },
-    { id: 13, naziv: "Matematika 13", ocjena: "A" },
-    { id: 14, naziv: "Matematika 14", ocjena: "A" },
-    { id: 15, naziv: "Matematika 15", ocjena: "A" },
-    { id: 16, naziv: "Matematika 16", ocjena: "A" },
-    { id: 17, naziv: "Matematika 17", ocjena: "A" }
-];
+import axios from "axios";
+import { useEffect } from "react";
 
 const GradeSheet = () => {
+
+
+
     const [currentPage, setCurrentPage] = useState(1);
+    const [ukupno,setUkupno] = useState();
+    const [polozeni,setPolozeni] = useState();
     const subjPerPage = 5;
 
-    // Calculate the indices of the lessons to be shown on the current page
-    const indexOfLastSubj = currentPage * subjPerPage;
-    const indexOfFirstSubj = indexOfLastSubj - subjPerPage;
-    const currentSubj = predmeti.slice(indexOfFirstSubj, indexOfLastSubj);
+
+    const fetchPolozeni = async () => {
+        try {
+            const response = await axios.get(`/polozeni/${currentPage}`);
+            setPolozeni(response.data);
+        }
+        catch(err) {
+            console.log(err);
+        }
+    
+    }
+
+    const fetchUkupanBroj = async () => {
+        try {
+            const response = await axios.get('/polozeni/brojPolozenih');
+            setUkupno(response.data[0].brojPolozenih);
+            console.log("USAO JE OVDJE",response.data);
+        }
+        catch(err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        fetchUkupanBroj();
+    },[])
+    
+    useEffect(() => {
+        fetchPolozeni();
+    },[currentPage])
+    
+
+    const izracunaj = (brojPoena) => {
+        
+        if(brojPoena >= 90) {
+            return 'A';
+        }
+        if(brojPoena >= 80) {
+            return 'B';
+        }
+        if(brojPoena >= 70) {
+            return 'C';
+        }
+        if(brojPoena >= 60) {
+            return 'D';
+        }
+        if(brojPoena >= 50) {
+            return 'E';
+        }
+
+
+    }
+
+ 
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     return (
@@ -49,21 +86,23 @@ const GradeSheet = () => {
                         <th>Predmet</th>
                         <th>Ocjena</th>
                         <th>ECTS</th>
+                        <th>Semestar</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {currentSubj.map((predmet) => (
-                        <tr key={predmet.id}>
-                            <td>{predmet.naziv}</td>
-                            <td>{predmet.ocjena}</td>
-                            <td>{predmet.ects}</td>
+                    {polozeni?.map((predmet) => (
+                        <tr>
+                            <td>{predmet.ime_predmeta}</td>
+                            <td>{izracunaj(predmet.broj_poena)}</td>
+                            <td>{predmet.broj_kredita}</td>
+                            <td>{predmet.broj_semestra}</td>
                         </tr>
                     ))}
                 </tbody>
             </table>
             <Pagination
                 itemsPerPage={subjPerPage}
-                totalItems={predmeti.length}
+                totalItems={ukupno}
                 paginate={paginate}
                 currentPage={currentPage}
             />
