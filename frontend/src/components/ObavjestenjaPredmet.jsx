a7ac4c185e448b506503889a2a51182fe5c9f324
Uroš Milović <urkemilovic16@gmail.com>
2024-06-27 16:49:50 +0200

    neprocitana

--- frontend/src/components/ObavjestenjaPredmet.jsx
+++ frontend/src/components/ObavjestenjaPredmet.jsx
@@ -8,23 +8,29 @@ import styles from '../styles/obavjestenjapredmet.css'
 import AuthContext from '../context/AuthContext';
 import { Link } from 'react-router-dom';
 
+
 const ObavjestenjePredmet = () => {
   const {imePredmeta,imeSmjera,imeFakulteta} = useParams();
 
   const [obavjestenja,setObavjestenja] = useState([]);
   const {user} = useContext(AuthContext);
+  const [neprocitana,setNeprocitana] = useState([]);
 
   const fetchObavjestenja = async () => {
     try {
 
       const response = await axios.get(`/obavjestenje/${imePredmeta}/${imeSmjera}/${imeFakulteta}`)
-      console.log(response.data);
+      const response2 = await axios.get(`/obavjestenje/neprocitanaObavjestenja/${imePredmeta}/${imeSmjera}/${imeFakulteta}`)
+      // console.log(response.data);
+
+      console.log(response2.data);
       
       setObavjestenja(response.data);
+      setNeprocitana(response2.data);
 
     }
     catch(err) {
-
+      console.log(err);
     }
   }
 
@@ -39,7 +45,7 @@ const ObavjestenjePredmet = () => {
       {user.rola === 'Profesor' && <button className = "okaciObavjestenje"><Link to = {`/newNotification/${imePredmeta}/${imeSmjera}/${imeFakulteta}`} id = "novoObavjestenjeLink">Okačite novo obavještenje</Link></button>}
 
       <div className = "obavjestenjaPredmet">
-        {obavjestenja.map((obavjestenje) => <Obavjestenje naslov = {obavjestenje.naslov} opis = {obavjestenje.opis} key = {obavjestenje.id_obavjestenja} id = {obavjestenje.id_obavjestenja}   datumKreiranja = {obavjestenje.datum_kreiranja}/>)}
+        {obavjestenja.map((obavjestenje) => <Obavjestenje setNeprocitana = {setNeprocitana} neProcitana = {neprocitana} naslov = {obavjestenje.naslov} opis = {obavjestenje.opis} key = {obavjestenje.id_obavjestenja} id = {obavjestenje.id_obavjestenja}   datumKreiranja = {obavjestenje.datum_kreiranja}/>)}
       </div>
 
     </div>
