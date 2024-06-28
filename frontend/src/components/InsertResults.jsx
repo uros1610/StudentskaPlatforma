ca2efb6aa56d87ecafe2114ad57c15c49b5ef6cb
Uroš Milović <urkemilovic16@gmail.com>
2024-06-28 20:16:37 +0200

    insert rezultata,funkcionalnosti implementirane

--- frontend/src/components/InsertResults.jsx
+++ frontend/src/components/InsertResults.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import { useState,useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
+import styles from '../styles/insertresults.css'
 
 const InsertResults = () => {
 
@@ -18,7 +19,8 @@ const InsertResults = () => {
 
     
 
-    const [studenti,setStudenti] = useState();
+    const [studenti,setStudenti] = useState([]);
+    const [rezultati,setRezultati] = useState([]);
 
     const {imePredmeta,imeSmjera,imeFakulteta} = useParams();
 
@@ -29,14 +31,19 @@ const InsertResults = () => {
     const handleChange = (e) => {
         const id = e.target.id;
 
-        setFilter({...filter,id:e.target.value})
+        const newObj = {...filter};
+
+        newObj[id] = e.target.value;
+
+        setFilter(newObj);
+        
         
     }
 
     const fetchStudenti = async () => {
         try {
             const response = await axios.get(`/student/${imePredmeta}/${imeSmjera}/${imeFakulteta}?indeks=${filter.indeks}&imeStudenta=${filter.imeStudenta}&prezimeStudenta=${filter.prezimeStudenta}`);
-            console.log(response);
+            // console.log(response);
             setStudenti(response.data);
         }
         catch (err) {
@@ -44,15 +51,41 @@ const InsertResults = () => {
         }
     }
 
+    const fetchRezultati = async () => {
+        try {
+            const response = await axios.get(`/profesor/sviRezultati/${imePredmeta}/${imeSmjera}/${imeFakulteta}`);
+            console.log(response);
+            setRezultati(response.data);
+        }
+        catch(err) {
+            console.log(err);
+        }
+    }
+
     useEffect(() => {
+       // console.log(filter);
         fetchStudenti();
+        fetchRezultati();
     },[filter])
 
   return (
-    <div>
-        <input id = "indeks" onChange={handleChange}/>
-        <input id = "ime" onChange={handleChange}/>
-        <input id = "prezime" onChange={handleChange}/>
+    <div id = "resultsDiv">
+
+        <div id = "divInputs">
+            <input id = "indeks" onChange={handleChange} placeholder = "Indeks"/>
+            <input id = "imeStudenta" onChange={handleChange} placeholder = "Ime studenta" />
+            <input id = "prezimeStudenta" onChange={handleChange} placeholder = "Prezime studenta" />
+        </div>
+
+
+        <div id = "allStudents">
+            {studenti?.map(student => (<div>{student.imeStudenta} {student.prezimeStudenta} {student.indeks}
+            
+                
+
+            </div>))}
+        </div>
+        
     </div>
   )
 }
