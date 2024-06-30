986f84e08a4397bd6b58d4f772b20e3b0a8b72fd
Uroš Milović <urkemilovic16@gmail.com>
2024-06-30 10:56:43 +0200

    izmjene u upitima za materijale

--- backend/controllers/materijal.js
+++ backend/controllers/materijal.js
@@ -41,7 +41,7 @@ exports.sviMaterijaliPredmet = (req, res) => {
     const ime_smjera = req.params.imeSmjera;
     const ime_fakulteta = req.params.imeFakulteta;
 
-    const query = `SELECT putanja FROM materijal WHERE ime_predmeta = ? AND ime_smjera = ? AND ime_fakulteta = ?`;
+    const query = `SELECT putanja FROM materijal WHERE ime_predmeta = ? AND ime_smjera = ? AND ime_fakulteta = ? ORDER BY datum_kreiranja DESC`;
 
     console.log(req.params);
 
@@ -139,51 +139,70 @@ exports.downloadMaterial = (req, res) => {
 };
 
   
+
 exports.okaciMaterijal = (req, res) => {
+    // Log the structure of req.files to debug
+    console.log(req.files);
+
     if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400).json({ error: 'No materials were attatched' });
+        return res.status(400).json({ error: 'No materials were attached' });
     }
 
-    const {ime_predmeta} = req.body;
-    const {ime_smjera} = req.body;
-    const {ime_fakulteta} = req.body;
-    const {ime_materijala} = req.body;
-
+    const { ime_predmeta, ime_smjera, ime_fakulteta, ime_fajla } = req.body;
+    console.log(req.body);
     const attached_file = req.files.file;
 
-    const putanja = '../public/'+ime_materijala;
-    const query = `INSERT INTO materijali (korisnickoime_profesora, ime_predmeta, ime smjera, ime_fakulteta, putanja, naslov) VALUES (?,?,?,?,?,?)`;
+    console.log(attached_file);
+
+    // Check if the attached_file is undefined
+    if (!attached_file) {
+        return res.status(400).json({ error: 'No file uploaded' });
+    }
+
+    const putanja = `./public/files/${attached_file.name}`;
+    const query = `INSERT INTO materijal (korisnickoime_profesora, ime_predmeta, ime_smjera, ime_fakulteta, putanja, naslov,datum_kreiranja) VALUES (?, ?, ?, ?, ?, ?,?)`;
 
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token,process.env.SECRET_KEY,(err,data) => {
-
-        if(err) {
+    jwt.verify(token, process.env.SECRET_KEY, (err, data) => {
+        if (err) {
             return res.status(403).json("Forbidden!");
         }
 
-        db.query(query,[data.korisnickoIme, ime_predmeta, ime_smjera, ime_fakulteta, putanja, ime_materijala], (err,results) => {
-            if (err) {
-                return res.status(500).json("Internal server error!");
-            }
-            
-            attached_file.mv(putanja, (err) => {
-                if (err) {
-                  return res.status(500).json({ error: 'File upload failed', err: err });
-                }
-            
-                res.status(200).json({
-                    message: 'File uploaded successfully',
-                    file: {
-                    name: attached_file.name,
-                    mimetype: attached_file.mimetype,
-                    size: attached_file.size
-                }});
-              });
-        });
-        
-    });
+        const q = `SELECT * FROM profesor_predmet WHERE korisnickoime_profesora = ? AND ime_predmeta = ? AND ime_smjera = ? AND ime_fakulteta = ?`;
 
-}
+        db.query(q, [data.korisnickoIme, ime_predmeta, ime_smjera, ime_fakulteta], (err, podaci) => {
+            if (err) {
+                return res.status(500).json(err);
+            }
+
+            if (podaci.length === 0) {
+                return res.status(403).json("Forbidden!");
+            }
+
+            db.query(query, [data.korisnickoIme, ime_predmeta, ime_smjera, ime_fakulteta, putanja, ime_fajla,new Date()], (err, results) => {
+                if (err) {
+                    return res.status(500).json(err);
+                }
+
+                attached_file.mv(putanja, (err) => {
+                    if (err) {
+                        return res.status(500).json({ error: 'File upload failed', err: err });
+                    }
+
+                    res.status(200).json({
+                        message: 'File uploaded successfully',
+                        file: {
+                            name: attached_file.name,
+                            mimetype: attached_file.mimetype,
+                            size: attached_file.size
+                        }
+                    });
+                });
+            });
+        });
+    });
+};
+
 
 exports.obrisiMaterijal = (req, res) => {
     const id = req.params.id;
