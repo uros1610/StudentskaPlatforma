c13657dad485e2450a4fd5c8deab28d499870bf6
Uroš Milović <urkemilovic16@gmail.com>
2024-07-06 00:08:01 +0200

    mala izmjena

--- backend/controllers/provjera.js
+++ backend/controllers/provjera.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const sveProvjereDatumi = (req,res) => {
 
     const {imePredmeta,imeSmjera,imeFakulteta} = req.params;
-    const query = "SELECT DISTINCT ime_predmeta,ime_smjera,ime_fakulteta,datum_odrzavanja,ime_provjere FROM Rezultat WHERE ime_predmeta = ? AND ime_smjera = ? AND ime_fakulteta = ?"
+    const query = "SELECT DISTINCT ime_predmeta,ime_smjera,ime_fakulteta,datum_odrzavanja,ime_provjere FROM Rezultat WHERE ime_predmeta = ? AND ime_smjera = ? AND ime_fakulteta = ? ORDER BY datum_odrzavanja"
 
     db.query(query,[imePredmeta,imeSmjera,imeFakulteta],(err,data) => {
 
