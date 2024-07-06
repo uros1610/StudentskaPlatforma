349bda74ecf7384a4df430d6c164cc333fc4313d
Tiikss <trakocevic7@gmail.com>
2024-07-06 21:04:43 +0200

    Moj chat

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+const db = require('./db.js');
+
 const express = require('express')
 const http = require('http')
 
@@ -15,7 +17,134 @@ app.use('/provjera',require('./routes/provjera'))
 app.use('/polozeni',require('./routes/polozeni'))
 app.use('/materijal',require('./routes/materijal'));
 
+const getPoints = (korisnicko_ime, predmet, callback) => {
+    const query = `
+        SELECT ukupan_broj_poena
+        FROM pohadja
+        WHERE korisnickoime_studenta = ? AND ime_predmeta = ?
+    `;
+    db.query(query, [korisnicko_ime, predmet], (err, results) => {
+      if (err) return callback(err);
+      callback(null, results.map(row => row.ukupan_broj_poena));
+    });
+};
 
+const getSubjects = (korisnicko_ime, callback) => {
+    const query = `
+        SELECT ime_predmeta
+        FROM pohadja
+        WHERE korisnickoime_studenta = ?
+    `;
+    db.query(query, [korisnicko_ime], (err, result) => {
+      if (err) return callback(err);
+      callback(null, result);
+    });
+};
+
+const getProfessor = (ime, prezime, callback) => {
+    const query = `
+        SELECT ime_predmeta FROM profesor p INNER JOIN profesor_predmet pp
+        ON p.korisnickoime = pp.korisnickoime_profesora WHERE p.ime_profesora = ? 
+        AND p.prezime_profesora = ?
+    `;
+    db.query(query, [ime, prezime], (err, result) => {
+      if (err) return callback(err);
+      callback(null, result);
+    });
+}
+
+const getMaterials = (predmet, callback) => {
+    const query = `
+        SELECT naslov FROM materijal WHERE ime_predmeta = ?
+    `;
+    db.query(query, [predmet], (err, result) => {
+        if (err) return callback(err);
+        callback(null, result);
+    });  
+};
+
+app.post('/chat/:korisnickoIme', async (req, res) => {
+    const { prompt } = req.body;
+    const korisnicko_ime = req.params.korisnickoIme;
+    console.log(korisnicko_ime);
+  
+    if (prompt.toLowerCase() === "zdravo!") {
+      return res.json({ reply: "Zdravo. Kako Vam mogu pomoći?" });
+    } else if (prompt.toLowerCase().includes("koliko bodova imam iz predmeta")) {
+  
+      const regex = /koliko bodova imam iz predmeta (.+)\?/i; 
+      const match = prompt.match(regex);
+      
+      if (match) {
+        const predmet = match[1].trim();
+  
+        getPoints(korisnicko_ime, predmet, (err, slots) => {
+          console.log(prompt.toLowerCase()); 
+          if (err) return res.status(500).send('Greška prilikom dohvatanja bodova.');
+          if (slots.length > 0) {
+            res.json({ reply: `Vaš broj bodova iz predmeta ${predmet} je: ${slots.join(', ')}.` });
+          } else {
+            res.json({ reply: `Nemate bodove iz tog predmeta.` });
+          }
+        });
+      } else {
+        res.status(400).send('Pogrešan format pitanja.');
+      }
+    } else if (prompt.toLowerCase().includes("koje predmete pohađam")) {
+      const regex = /koje predmete pohađam\?/i; 
+      const match = prompt.match(regex);
+  
+      if (match) {  
+        getSubjects(korisnicko_ime, (err, result) => {
+          if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Greška prilikom pretraživanja podataka.' });
+          }
+          res.status(200).json({ reply: `Vaši predmeti su: ${result.map(row => row.ime_predmeta).join(', ')}.` });
+        });
+      } else {
+        res.status(400).send('Pogrešan format pitanja.');
+      }
+    } else if (prompt.toLowerCase().includes("svi predmeti koje predaje profesor")) {
+      const regex = /svi predmeti koje predaje profesor (.+)\?/i; 
+      const match = prompt.match(regex);
+  
+      if (match) {
+        const profesor = match[1].trim();
+        const [ime, prezime] = profesor.split(' ');
+
+        getProfessor(ime, prezime, (err, result) => {
+          if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Greška prilikom pretraživanja podataka.' });
+          }
+          res.status(200).json({ reply: `Profesor ${profesor} predaje predmete: ${result.map(row => row.ime_predmeta).join(', ')}.` });
+        });
+      } else {
+        res.status(400).send('Pogrešan format pitanja.');
+      }
+    } else if (prompt.toLowerCase().includes("koji su dostupni materijali za predmet")) {
+        const regex = /koji su dostupni materijali za predmet (.+)\?/i; 
+        const match = prompt.match(regex);
+    
+        if (match) {
+            const predmet = match[1].trim();
+    
+            getMaterials(predmet, (err, result) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Greška prilikom pretraživanja podataka.' });
+            }
+            res.status(200).json({ reply: `Dostupni materijali za predmet ${predmet} su: ${result.map(row => row.naslov).join(', ')}.` });
+            });
+        } else {
+            res.status(400).send('Pogrešan format pitanja.');
+        }
+    }
+    else {
+      res.json({ reply: "Ne mogu da odgovorim na Vaše pitanje." });
+    }
+  });
 
 server.listen(8000,() => {
     console.log("Connected!");
--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,11 +1,109 @@
 import React from "react";
+import { useState } from "react";
+import "../styles/chat.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRobot } from '@fortawesome/free-solid-svg-icons'
+import axios from "axios";
+import { useEffect } from "react";
+import { useRef } from "react";
+import AuthContext from '../context/AuthContext'
+import { useContext } from "react";
 
-const Chat = () => {
+function Chat() {
+    const { user } = useContext(AuthContext);
+    const [message, setMessage] = useState('');
+    const [chat, setChat] = useState([]);
+    const [suggestions, setSuggestions] = useState([
+        'Zdravo!',
+        'Koliko bodova imam iz predmeta [predmet]?',
+        'Koji su dostupni materijali za predmet [predmet]?',
+        'Koje predmete pohađam?',
+        'Svi predmeti koje predaje profesor [profesor]?'
+    ]);
+    const chatContainerRef = useRef(null);
+
+    useEffect(() => {
+        chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+    }, [chat]);
+
+    const sendMessage = async () => {
+        if (message.trim() === '') return; // Check for empty message
+
+        const userMessage = { sender: 'user', text: message };
+        setChat(prevChat => [...prevChat, userMessage]);
+
+        try {
+            const response = await axios.post(`/chat/${user.korisnickoIme}`, { prompt: message });
+            const botMessage = { sender: 'bot', text: response.data.reply };
+            setChat(prevChat => [...prevChat, botMessage]);
+        } catch (error) {
+            console.error('Došlo je do greške pri slanju poruke:', error);
+            const errorMessage = { sender: 'bot', text: 'Došlo je do greške. Pokušajte ponovo kasnije.' };
+            setChat(prevChat => [...prevChat, errorMessage]);
+        }
+
+        setMessage('');
+    };
+
+    const handleSuggestionClick = (suggestion) => {
+        setMessage(suggestion);
+    };
+
+    const handleInputChange = (event) => {
+        setMessage(event.target.value);
+    };
+
     return (
-        <div>
-        <h1>Chat</h1>
-        </div>
+        <main id="chat-body">
+            <div id="chat-title">
+                <FontAwesomeIcon icon={faRobot} className="home-icons" />
+                <h1>Chat</h1>
+            </div>
+            <div style={{ maxWidth: '500px', margin: 'auto', padding: '20px', border: '1px solid #ccc', borderRadius: '10px', marginBlock: '30px' }}>
+            <div id="chat-title">
+                <FontAwesomeIcon icon={faRobot} className="home-icons" />
+                <h1>Chat</h1>
+            </div>
+            <div ref={chatContainerRef} style={{ height: '300px', overflowY: 'auto', marginBottom: '10px', marginTop: '15px', position: 'relative' }}>
+                <div style={{ position: 'absolute', bottom: 0, width: '100%' }}></div>
+                {chat.map((message, index) => (
+                <div key={index} style={{ display: 'flex', justifyContent: message.sender === 'user' ? 'flex-end' : 'flex-start', marginBottom: '10px' }}>
+                    <p style={{
+                    maxWidth: '80%',
+                    wordWrap: 'break-word',
+                    margin: '5px',
+                    padding: '10px',
+                    borderRadius: '5px',
+                    backgroundColor: message.sender === 'bot' ? '#F4B392' : '#F4B392',
+                    color: message.sender === 'bot' ? '#000' : '#fff'
+                    }}>
+                    {message.sender === 'user' ? 'Korisnik: ' : 'Bot: '}
+                    {message.text}
+                    </p>
+                </div>
+                ))}
+            </div>
+            <div style={{ marginBottom: '10px', textAlign: 'center' }}>
+                {suggestions.map((suggestion, index) => (
+                <button key={index} onClick={() => handleSuggestionClick(suggestion)} style={{ marginRight: '10px', marginBottom: '10px'}}>
+                    {suggestion}
+                </button>
+                ))}
+            </div>
+            <div style={{ display: 'flex', marginBottom: '10px' }}>
+                <textarea
+                rows={1}
+                cols={50}
+                value={message}
+                onChange={handleInputChange}
+                placeholder="Napišite vaš upit ovdje..."
+                style={{ width: '100%', padding: '5px', borderRadius: '5px', resize: 'none' }}
+                />
+                <button onClick={sendMessage} style={{ padding: '10px 20px', backgroundColor: '#F4B392', color: '#fff', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>Pošalji upit</button>
+            </div>
+            </div>
+        </main>
     );
-};
+  }
 
 export default Chat;
