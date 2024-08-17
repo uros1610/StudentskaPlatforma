430a352d896178b5fe7b754be43d1e73ee35a937
uros1610 <urkemilovic16@gmail.com>
2024-08-17 23:22:37 +0200

    db

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,12 @@
 var mysql = require('mysql')
 
- const dbConf = mysql.createConnection({
-    host:"localhost",
-    user:'root',
-    password:'1234',
-    database:'StudentskaPlatforma',
+const dbConf = mysql.createConnection({
+    host:process.env.DATABASE_HOST,
+    user:process.env.DATABASE_USER,
+    password:process.env.DATABASE_PASSWORD,
+    database:process.env.DATABASE_NAME,
 })
+
 
 
 
